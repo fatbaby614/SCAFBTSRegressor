0ed5152f54da6a33bba85886c465cad167448999
TanHuang <fatbaby@163.com>
2026-05-13 13:53:23 +0800

    Delete _find_line.py

--- _find_line.py
+++
@@ -1,5 +0,0 @@
-from config import PAPER_DIR
-lines = open(os.path.join(PAPER_DIR, 'paper_jne.tex'), encoding='utf-8').readlines()
-for i, l in enumerate(lines):
-    if '\\section' in l or '\\subsection' in l:
-        print(f"L{i+1}: {l.rstrip()[:120]}")
